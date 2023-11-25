07b1e7bbe78288dba76da6b261b2d1f9e9c0bf10
Jakub Karaš <jakub.karas.2@student.tuke.sk>
2023-11-25 23:37:52 +0100

    update

--- backend/routes/reservation.js
+++ backend/routes/reservation.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const client = require('../database/databasepg');
 const bcrypt = require('bcrypt');
+const schedule = require('node-schedule');
 
 router.get('/', (req, res) => {
     const sql = 'SELECT * FROM reservation';
