c8f9e0137121fd23e86471c585860ab4b42bc5d1
Jakub Karaš <jakub.karas.2@student.tuke.sk>
2023-11-26 04:11:25 +0100

    update

--- backend/routes/strike.js
+++ backend/routes/strike.js
@@ -25,6 +25,20 @@ router.get('/id/:id', (req, res) => {
     });
 });
 
+router.get('/', (req, res) => {
+    const thirtyDaysAgo = new Date();
+    thirtyDaysAgo.setDate(thirtyDaysAgo.getDate() - 30);
+
+    const sql = 'SELECT * FROM strike WHERE created_at >= $1';
+    client.query(sql, [thirtyDaysAgo], (err, result) => {
+        if (err) {
+            res.status(500).json({ error: err });
+        } else {
+            res.status(200).json(result.rows);
+        }
+    });
+});
+
 router.get('/employee_id/:id', (req, res) => {
     const sql = `SELECT * FROM strike WHERE employee_id = ${req.params.id}`;
     client.query(sql, (err, result) => {
